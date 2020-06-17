634cac9aa53b4526d4bc60d3fa86afe288248934
Alex Oleynichenko <oleynichenkos@gmail.com>
2020-06-17 12:16:03 +0300

    add browser router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,31 @@
 import WelcomeScreen from "@components/welcome-screen/welcome-screen";
+import ArtistQuestionScreen from "@components/genre-question-screen/genre-question-screen";
+import GenreQuestionScreen from "@components/question-artist-screen/question-artist-screen";
+import {Switch, BrowserRouter, Route} from "react-router-dom";
 
 const welcomeButtonHandler = () => {};
 
 const App = (props) => {
   const {errorCount} = props;
 
-  return <WelcomeScreen
-    errorCount={errorCount}
-    onWelcomeButtonClick={welcomeButtonHandler}
-  />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorCount={errorCount}
+            onWelcomeButtonClick={welcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
